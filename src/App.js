e1ae558a502f71a57cfd549d702e329e11847f9c
lokesh190496 <lokeshvasu63@gmail.com>
2025-08-03 08:42:28 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
@@ -6,10 +5,14 @@ import Resume from './pages/Resume/Resume';
 import Portfolio from './pages/Portfolio/Portfolio';
 import Skills from './pages/Skills/Skills';
 import Contact from './pages/Contact/Contact';
+import AppNavbar from './components/AppNavbar/AppNavbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return (
     <div className="App">
+      <AppNavbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ src/components/AppNavbar/AppNavbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const AppNavbar = () => {
+  return (
+    <Navbar bg="light" expand="lg" className="shadow-sm">
+      <Container>
+        {/* Logo on the left */}
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src="/logo.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="React Logo"
+          />{' '}
+        </Navbar.Brand>
+
+        {/* Responsive toggle */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        {/* Nav Links on the right */}
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
+            <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link as={Link} to="/skills">Skills</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default AppNavbar;
